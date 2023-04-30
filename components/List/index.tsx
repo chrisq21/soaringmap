e71bc2c2a905ba0d9803c4afd12976adfc10f85b
Chris Queen <chrisqueen10@gmail.com>
2023-04-30 13:30:00 -0400

    Add search

--- components/List/index.tsx
+++ components/List/index.tsx
@@ -1,7 +1,12 @@
+import {useState} from 'react'
 import styles from './List.module.css'
 import Accordion from 'react-bootstrap/Accordion'
+import {Form, InputGroup} from 'react-bootstrap'
+import {BsSearch} from 'react-icons/bs'
 
 export default ({items, map, handleClick}) => {
+  const [searchQuery, setSearchQuery] = useState('')
+
   function groupObjectsByState(objects) {
     const groups = {}
     for (const obj of objects) {
@@ -22,14 +27,33 @@ export default ({items, map, handleClick}) => {
 
   const itemsByState = groupObjectsByState(items)
 
+  const searchResults = items.filter((item) => {
+    const title = item.title.toLowerCase()
+    const query = searchQuery.toLowerCase()
+    return title.includes(query)
+  })
+
   return (
     <div className={styles.container}>
-      <Accordion>
-        {itemsByState.map((group, index) => (
-          <Accordion.Item eventKey={String(index)} key={index}>
-            <Accordion.Header className={styles.category}>{group.state}</Accordion.Header>
-            {group.items.map((item, index) => (
-              <Accordion.Body key={index}>
+      <InputGroup>
+        <InputGroup.Text id="searchIcon">
+          <BsSearch />
+        </InputGroup.Text>
+        <Form.Control
+          type="text"
+          placeholder="Search by title"
+          aria-label="search by title"
+          aria-describedby="searchIcon"
+          onChange={(e) => {
+            setSearchQuery(e.target.value)
+          }}
+        />
+      </InputGroup>
+      {searchQuery && (
+        <div className={styles.searchContainer}>
+          <div className={styles.searchResults}>
+            {searchResults.map((item, index) => {
+              return (
                 <button
                   key={index}
                   className={styles.itemTitle}
@@ -44,11 +68,38 @@ export default ({items, map, handleClick}) => {
                 >
                   {item.title}
                 </button>
-              </Accordion.Body>
-            ))}
-          </Accordion.Item>
-        ))}
-      </Accordion>
+              )
+            })}
+          </div>
+        </div>
+      )}
+      {!searchQuery && (
+        <Accordion>
+          {itemsByState.map((group, index) => (
+            <Accordion.Item eventKey={String(index)} key={index}>
+              <Accordion.Header className={styles.category}>{group.state}</Accordion.Header>
+              {group.items.map((item, index) => (
+                <Accordion.Body key={index}>
+                  <button
+                    key={index}
+                    className={styles.itemTitle}
+                    onClick={() => {
+                      const currentZoom = map.current.getZoom()
+                      handleClick(item)
+                      map.current.easeTo({
+                        center: item.coordinates,
+                        zoom: currentZoom < 5.3 ? 5.3 : currentZoom,
+                      })
+                    }}
+                  >
+                    {item.title}
+                  </button>
+                </Accordion.Body>
+              ))}
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      )}
     </div>
   )
 }
